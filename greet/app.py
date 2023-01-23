ba3b27fdbd20831491c7ecd8fb85ca8cf50b3f73
DNS1196 <107742954+DNS1196@users.noreply.github.com>
2023-01-22 23:15:26 -0500

    Add files via upload

---
+++ greet/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/welcome')
+def main_welcome():
+    return "welcome"
+
+@app.route('/welcome/home')
+def home_welcome():
+    return "welcome home"
+
+@app.route('/welcome/back')
+def back_welcome():
+    return "welcome back"
